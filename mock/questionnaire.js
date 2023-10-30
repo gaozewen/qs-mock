0442533f7405d71eac1a9c0308c6eb0df06ccd94
gaozewen <1440651163@qq.com>
2023-10-30 23:30:48 +0800

    feat: 新增更新、复制、批量彻底删除问卷3个 mock 接口

--- mock/questionnaire.js
+++ mock/questionnaire.js
@@ -38,16 +38,50 @@ module.exports = [
     url: "/api/questionnaire",
     method: "get",
     response(ctx) {
-      const { url = "" } = ctx;
+      const { url = "", query = {} } = ctx;
       const isStar = url.indexOf("isStar=true") >= 0;
       const isDeleted = url.indexOf("isDeleted=true") >= 0;
+      const pageSize = parseInt(query.pageSize) || 10;
       return {
         errno: 0,
         data: {
-          list: getQuestionnaireList({ isStar, isDeleted }),
+          list: getQuestionnaireList({ isStar, isDeleted, len: pageSize }),
           total: 100, // 总条数
         },
       };
     },
   },
+  {
+    // 更新问卷
+    url: "/api/questionnaire/:id",
+    method: "patch",
+    response() {
+      return {
+        errno: 0,
+      };
+    },
+  },
+  {
+    // 复制问卷
+    url: "/api/questionnaire/duplicate/:id",
+    method: "post",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+        },
+      };
+    },
+  },
+  {
+    // 批量彻底删除问卷
+    url: '/api/questionnaire',
+    method: 'delete',
+    response() {
+      return {
+        errno: 0
+      }
+    }
+  }
 ];
