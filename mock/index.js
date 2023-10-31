1fb995c782faa9f154825d31572ba9af35540451
gaozewen <1440651163@qq.com>
2023-10-31 17:46:22 +0800

    feat: 新增获取用户信息、注册、登录 mock 接口

--- mock/index.js
+++ mock/index.js
@@ -1,6 +1,7 @@
 const test = require("./test");
 const questionnaire = require("./questionnaire");
+const user = require("./user");
 
-const mockList = [...test, ...questionnaire];
+const mockList = [...test, ...questionnaire,...user];
 
 module.exports = mockList;
