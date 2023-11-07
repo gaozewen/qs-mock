00036dd405a8127c0d320ac174f729cfda05e873
gaozewen <1440651163@qq.com>
2023-11-07 11:12:09 +0800

    feat: 新增单个组件的数据统计接口

--- mock/statistic.js
+++ mock/statistic.js
@@ -7,7 +7,7 @@ const Random = Mock.Random;
 module.exports = [
   {
     // 统计列表
-    url: "/api/statistic/:id",
+    url: "/api/statistic/:questionnaireId",
     method: "get",
     response(ctx) {
       const { query = {} } = ctx;
@@ -17,6 +17,23 @@ module.exports = [
         data: {
           total: 100, // 分页
           list: getStatisticList(pageSize),
+        },
+      };
+    },
+  },
+  {
+    // 获取单个组件的统计数据汇总
+    url: "/api/statistic/:questionnaireId/:componentId",
+    method: "get",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          statistic: [
+            { name: "选项1", count: 20 },
+            { name: "选项2", count: 10 },
+            { name: "选项3", count: 25 },
+          ],
         },
       };
     },
